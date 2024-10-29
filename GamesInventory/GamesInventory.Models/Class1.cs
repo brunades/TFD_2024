cdd816363358a43076a7047132943a6248cec24a
brunades <bruna.contino@outlook.com>
2024-10-29 09:48:41 +0100

    first commit

---
+++ GamesInventory/GamesInventory.Models/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace GamesInventory.Models;
+
+public class Class1
+{
+
+}
---
+++ GamesInventory/GamesInventory.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GamesInventory.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
